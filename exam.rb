c1eba0227ba8afcc60a06330a322a927c8ac6883
Neil Northrop <nnorthrop@gmail.com>
2013-06-14 09:50:12 -0700

    added comments to exam.rb and exam_test.rb

--- exam.rb
+++ exam.rb
@@ -1,10 +1,10 @@
 # Pushed to github under PCSWeek2Exam
 require 'pp'
-
+# Started a class to hold methods
 class Neil
 
 	attr_accessor :fun_rating
-
+	# Started a method to do the FizzBuzz challenge
 	def fizzbuzz(num)
 		0.upto(num) do |i|
 			if i % 3 == 0 && i % 5 == 0
@@ -18,12 +18,12 @@ class Neil
 			end
 		end
 	end
-
+	# A method to add to the fun rating of this class
 	def add_fun_rating(fun_rating)
 		fun_rating = fun_rating + 1
 		return
 	end
-
+	# A method to talk to me and I'll talk back (very limited talking back)
 	def talk_to(response, game_response)
 		puts 'How are you?'
 		response = response.downcase.chomp
@@ -32,6 +32,7 @@ class Neil
 				play_game = game_response
 				if play_game == 'yes'
 					puts 'Tubular'
+					# Added to the fun rating!
 					add_fun_rating(1)
 					return true
 				elsif play_game == 'no'
@@ -48,12 +49,15 @@ class Neil
 			puts 'Too bad'
 			return true
 		else
+			# Used recursive looping
 			talk_to('good', 'yes')
 		end
 	end
 
 end
 
+# Created an instance of the class
 neil = Neil.new
+# Used a method on the instance neil
 neil.fizzbuzz(10)
-neil.talk_to('Good', 'yes')
+neil.talk_to('run', 'yes')
--- exam_test.rb
+++ exam_test.rb
@@ -1,17 +1,17 @@
 require 'pp'
 require 'minitest/autorun'
 require './exam.rb'
-
+# Set up my test class using my Sublime snippet 'mt'!
 class NeilTest < MiniTest::Unit::TestCase
-
+	# Used the setup method to add my instance class
 	def setup
 		@neil = Neil.new
 	end
-
+	# Created a test to make a class
 	def test_that_it_makes_a_class
 		assert Neil.new
 	end
-
+	# A test to talk to me
 	def test_that_neil_can_talk_to_you
 		assert @neil.talk_to('good', 'yes')
 	end
